7dbff09aa1eb2820f7a3f090ad0c18cab4aac7a9
Ruben Jubeh <ruben.jubeh@oth-regensburg.de>
2024-06-05 16:04:22 +0200

    current state

--- src/main/java/pg2in/NodeViz.java
+++ src/main/java/pg2in/NodeViz.java
@@ -17,17 +17,19 @@ public class NodeViz extends SimpleGraphicsApp {
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 800;
     public static final int RADIUS = 8;
-    public static final int ANIM_DELAY = 10;
+    public static final int ANIM_DELAY = 20;
     public static final int LABEL_X_OFFSET = 15;
     public static final int LABEL_Y_OFFSET = -10;
 
-    Map<Character, VizSensorNode> nodes = new LinkedHashMap<>();
+    Map<Character, VizSensorNode> nodes = new LinkedHashMap<>(); // TODO color mapping
 
     @Override
     public void initialize() {
         setCanvasSize(WIDTH, HEIGHT);
         setBackgroundColor(Colors.BLACK);
-        nodes.put('A', new VizSensorNode('A'));
+        for (char c = 'A'; c <= 'Z'; c++) {
+            nodes.put(c, new VizSensorNode(c));
+        }
         new Thread(() -> {
             try {
                 var bis = new BufferedReader(new InputStreamReader(System.in));
@@ -42,12 +44,11 @@ public class NodeViz extends SimpleGraphicsApp {
         }).start();
     }
 
-    // A 172 32 -1040 2667 21 0 0 0 0 0:-57
     private void parseLine(String line) {
         char id = line.charAt(0);
-        VizSensorNode vsn = nodes.get(id);
+        var vsn = nodes.get(id);
         if (vsn != null) {
-            vsn.parse(line.substring(2));
+            vsn.parse(line);
         }
     }
 
@@ -65,14 +66,20 @@ public class NodeViz extends SimpleGraphicsApp {
     class VizSensorNode {
 
         char key;
-        int dx = 1, dy = 2;
+        float dx = (float) (Math.random() * .15) * (Math.random() > 0.5 ? 1 : -1);
+        float dy = (float) (Math.random() * .15) * (Math.random() > 0.5 ? 1 : -1);
         Circle ball;
         Label label;
 
         public VizSensorNode(char key) {
             this.key = key;
-            ball = add(new Circle(0, 0, RADIUS, Colors.BLUE));
-            label = add(new Label(LABEL_X_OFFSET, LABEL_Y_OFFSET, ""+key));
+            int id = key - 'A';
+            int gridX = id % 5;
+            int gridY = id / 5;
+            float px = (gridX+1) * WIDTH / 6f;
+            float py = (gridY+1) * HEIGHT / 6f;
+            ball = add(new Circle(px, py, RADIUS, Colors.BLUE));
+            label = add(new Label(px + LABEL_X_OFFSET, py + LABEL_Y_OFFSET, "" + key));
         }
 
         void anim() {
@@ -86,25 +93,22 @@ public class NodeViz extends SimpleGraphicsApp {
             label.setPosition(ball.getXPos() + LABEL_X_OFFSET, ball.getYPos() + LABEL_Y_OFFSET);
         }
 
-        // 172 32 -1040 2667 21 0 0 0 0 0:-57
+        // A -8 564 -892 29 0 0 0 -33
         public void parse(String sensorLine) {
 
             String[] tokens = sensorLine.split(" ");
-            if (tokens.length < 11) {
+            if (tokens.length < 9) {
                 System.err.println("cannot parse: " + sensorLine);
                 return;
             }
-            int accX = Integer.parseInt(tokens[0]);
-            int accY = Integer.parseInt(tokens[1]);
-            int accZ = Integer.parseInt(tokens[2]);
-            int temp = Integer.parseInt(tokens[3]);
-            int lightLevel = Integer.parseInt(tokens[4]);
-            int soundLevel = Integer.parseInt(tokens[5]);
-            int buttonA = Integer.parseInt(tokens[6]);
-            int buttonB = Integer.parseInt(tokens[7]);
-            int touch1 = Integer.parseInt(tokens[8]);
-            int touch2 = Integer.parseInt(tokens[9]);
-            //int touch3 = Integer.parseInt(tokens[10]); // TODO replace : with space
+            int accX = Integer.parseInt(tokens[1]);
+            int accY = Integer.parseInt(tokens[2]);
+            int accZ = Integer.parseInt(tokens[3]);
+            int temp = Integer.parseInt(tokens[4]);
+            int buttonA = Integer.parseInt(tokens[5]);
+            int buttonB = Integer.parseInt(tokens[6]);
+            int touchLogo = Integer.parseInt(tokens[7]);
+            int rssi = Integer.parseInt(tokens[8]);
 
             // TODO
 
